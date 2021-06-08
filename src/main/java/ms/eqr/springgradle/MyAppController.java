18371e840262d030ef27a6c3ae2b768e64cb3eb5
Hari Mohan <hari.mohan@accolite.com>
2021-06-08 11:58:07 +0530

    hithub message

--- src/main/java/ms/eqr/springgradle/MyAppController.java
+++ src/main/java/ms/eqr/springgradle/MyAppController.java
@@ -9,7 +9,7 @@ public class MyAppController {
 
 	@RequestMapping("/")
 	public String index() {
-		return "Welcome to Gradle Spring Boot Docker App!";
+		return "Welcome to Gradle Github Spring Boot Docker App!";
 	}
 
 	@RequestMapping("/testSpring")
